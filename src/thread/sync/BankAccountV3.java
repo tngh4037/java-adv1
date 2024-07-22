ae9ff32bb06c2476173f36b68620d9b03d480b41
tngh4037 <tngh4037@gmail.com>
2024-07-23 04:06:33 +0900

    section6: synchronized 코드 블럭

---
+++ src/thread/sync/BankAccountV3.java
@@ -0,0 +1,43 @@
+package thread.sync;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV3 implements BankAccount {
+
+    private int balance;
+
+    public BankAccountV3(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+
+        synchronized (this) {
+            // == 임계 영역 시작 ==
+            // 검증: 잔고가 출금액 보다 적으면 진행하면 안됨
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                return false;
+            }
+
+            // 잔고가 출금액 보다 많으면 진행
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+            sleep(1000); // 출금을 처리하는 시간이 1초정도 걸린다고 가정
+            balance = balance - amount;
+            log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+            // == 임계 영역 종료 ==
+        }
+
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public synchronized int getBalance() {
+        return balance;
+    }
+}
