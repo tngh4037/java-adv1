98817e5f1acebfea88f47eef3d570f8eadb387f8
tngh4037 <tngh4037@gmail.com>
2024-07-25 04:40:29 +0900

    section8: Object - wait, notify - 예제3 코드

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -11,7 +11,8 @@ public class BoundedMain {
     public static void main(String[] args) {
         // 1. BoundedQueue 선택
         // BoundedQueue queue = new BoundedQueueV1(2); // 버퍼에는 2개까지 담을 수 있도록 하자.
-        BoundedQueue queue = new BoundedQueueV2(2); // 버퍼에는 2개까지 담을 수 있도록 하자.
+        // BoundedQueue queue = new BoundedQueueV2(2); // 버퍼에는 2개까지 담을 수 있도록 하자.
+        BoundedQueue queue = new BoundedQueueV3(2); // 버퍼에는 2개까지 담을 수 있도록 하자.
 
         // 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택해서 실행하자.
         // producerFirst(queue); // 2-1) 생산자 먼저 실행
