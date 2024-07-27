fcc2567b23bebf33de797c17e02ff5271e88184d
tngh4037 <tngh4037@gmail.com>
2024-07-27 09:37:27 +0900

    section10: CAS 락 구현2

--- src/thread/cas/spinlock/SpinLockMain.java
+++ src/thread/cas/spinlock/SpinLockMain.java
@@ -1,11 +1,13 @@
 package thread.cas.spinlock;
 
 import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
 
 public class SpinLockMain {
 
     public static void main(String[] args) {
-        SpinLockBad spinLock = new SpinLockBad();
+        // SpinLockBad spinLock = new SpinLockBad();
+        SpinLock spinLock = new SpinLock();
 
         Runnable task = new Runnable() {
             @Override
@@ -15,6 +17,7 @@ public class SpinLockMain {
                 try {
                     // critical section
                     log("비즈니스 로직 실행");
+                    // sleep(10); // 참고) 오래 걸리는 로직에서는 스핀 락 사용하면 안된다. ( 락을 기다리는 스레드가 CPU를 계속 사용하면서 반복문 계속 탐. )
                 } finally {
                     spinLock.unlock();
                 }
