85446a5baa662a5b380d5d6d2c5df02c029b52a7
tngh4037 <tngh4037@gmail.com>
2024-07-29 17:53:15 +0900

    section12: 문제와 풀이 (수정 후)

--- src/thread/executor/test/OldOrderServiceTestMain.java
+++ src/thread/executor/test/OldOrderServiceTestMain.java
@@ -1,11 +1,14 @@
 
 package thread.executor.test;
 
+import java.util.concurrent.ExecutionException;
+
 public class OldOrderServiceTestMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         String orderNo = "Order#1234"; // 예시 주문 번호
-        OldOrderService orderService = new OldOrderService();
+        // OldOrderService orderService = new OldOrderService();
+        NewOrderService orderService = new NewOrderService();
         orderService.order(orderNo);
     }
 }
