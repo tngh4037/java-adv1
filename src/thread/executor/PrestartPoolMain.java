763121bf541a80501a445f7c3fb8e60c7299bb05
tngh4037 <tngh4037@gmail.com>
2024-07-30 18:09:46 +0900

    section13: Executor 스레드 풀 관리 - 분석

---
+++ src/thread/executor/PrestartPoolMain.java
@@ -0,0 +1,17 @@
+package thread.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class PrestartPoolMain {
+
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newFixedThreadPool(1000);
+        ExecutorUtils.printState(es);
+
+        ThreadPoolExecutor poolExecutor = (ThreadPoolExecutor) es;
+        poolExecutor.prestartAllCoreThreads();
+        ExecutorUtils.printState(es);
+    }
+}
